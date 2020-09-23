8946fddf525ba4bf864fd9099a31083f879bb52c
ibalram <balramsinghrathore3@gmail.com>
2020-09-23 23:33:39 +0530

    // usecase1

--- src/com/capgemini/EmpWageBuilder.java
+++ src/com/capgemini/EmpWageBuilder.java
@@ -1,9 +1,20 @@
 package com.capgemini;
 
+
+// usecase1
+// Check employee is present or not
+
 public class EmpWageBuilder {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		int IS_FULL_TIME = 1;
+		
+		int empCheck = (int) Math.floor(Math.random() *10)%2;
+		
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
