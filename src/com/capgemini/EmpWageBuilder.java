1aad06574873bfcba86df1f4dd9cb529acd1babc
ibalram <balramsinghrathore3@gmail.com>
2020-09-24 00:01:55 +0530

    added usecase5 - calc wages for a month assuming 20 working days in a month

--- src/com/capgemini/EmpWageBuilder.java
+++ src/com/capgemini/EmpWageBuilder.java
@@ -1,33 +1,39 @@
 package com.capgemini;
 
 
-// usecase4
-// Solving using Case Statement
+// usecase5
+// Calculating wages for a month assuming 20 working days in a month
 
 public class EmpWageBuilder {
 
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int IS_FULL_TIME = 2;
 	public static final int IS_PART_TIME = 1;
+	public static final int NUM_OF_WORKING_DAYS = 2;
 	
 	public static void main(String[] args) {
 		
 		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
 		
-		int empCheck = (int) Math.floor(Math.random()*10)%3;
-		switch (empCheck) {
-			case IS_PART_TIME:
-				empHrs=4;
-				break;
-			case IS_FULL_TIME:
-				empHrs=8;
-				break;
-			default:
-				break;
+		for( int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+			int empCheck = (int) Math.floor(Math.random()*10)%3;
+			switch (empCheck) {
+				case IS_PART_TIME:
+					empHrs=4;
+					break;
+				case IS_FULL_TIME:
+					empHrs=8;
+					break;
+				default:
+					break;
+			}
+			empWage = EMP_RATE_PER_HOUR*empHrs;
+			totalEmpWage += empWage;
+			System.out.println("Emp Wage: " + empWage);
 		}
-		empWage = EMP_RATE_PER_HOUR*empHrs;
-		System.out.println("Emp Wage: " + empWage);
+		System.out.println("Total Emp Wage: "+totalEmpWage);
 	}
 
 }
