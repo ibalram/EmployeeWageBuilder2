7305614134d0dba32205fcc5a126d00ea9d25551
ibalram <balramsinghrathore3@gmail.com>
2020-09-24 00:08:20 +0530

    added corrction

--- src/com/capgemini/EmpWageBuilder.java
+++ src/com/capgemini/EmpWageBuilder.java
@@ -11,14 +11,16 @@ public class EmpWageBuilder {
 	public static final int IS_FULL_TIME = 2;
 	public static final int IS_PART_TIME = 1;
 	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MONTH = 10;
 	
 	public static void main(String[] args) {
 		
 		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpWage = 0;
-		
-		for( int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS) {
+			
+			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random()*10)%3;
 			switch (empCheck) {
 				case IS_PART_TIME:
@@ -30,10 +32,10 @@ public class EmpWageBuilder {
 				default:
 					break;
 			}
-			empWage = EMP_RATE_PER_HOUR*empHrs;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage: " + empWage);
+			totalEmpHrs+=empHrs;
+			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: "+empHrs);
 		}
+		int totalEmpWage = totalEmpHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage: "+totalEmpWage);
 	}
 
