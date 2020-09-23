3df49f6ab12fec30f6fdda20e61c4ddb4b57d7e3
ibalram <balramsinghrathore3@gmail.com>
2020-09-23 23:52:57 +0530

    added usecase2 - Calculate daily employee wage

--- src/com/capgemini/EmpWageBuilder.java
+++ src/com/capgemini/EmpWageBuilder.java
@@ -1,20 +1,21 @@
 package com.capgemini;
 
 
-// usecase1
-// Check employee is present or not
+// usecase2
+// Calculate Daily Employee Wage
 
 public class EmpWageBuilder {
 
 	public static void main(String[] args) {
+		int EMP_RATE_PER_HOUR = 20;
 		int IS_FULL_TIME = 1;
 		
-		int empCheck = (int) Math.floor(Math.random() *10)%2;
-		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
-	
+		double check = Math.floor(Math.random()*10)%2;
+		int dayHour = 0;
+		if (check==IS_FULL_TIME)
+			dayHour = 8;
+		int dailyWage = EMP_RATE_PER_HOUR*dayHour;
+		System.out.println("Emp Wage: " + dailyWage);
 	}
 
 }
